5ee9ab2df051c1b3a743e3f54add103b1bc84c5f
hxxvyweight <linuxtest923@gmail.com>
2026-07-13 10:39:15 +1000

    feat: implement sector seeking with SeekFrom

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,32 @@ fn main() -> std::io::Result<()> {
 
     let mut buffer: [u8; 512] = [0; 512];
 
-    let mut iso_file = File::open("../Akai CD-ROM Sound Library Volume 8.iso").expect("Failed to open file");
+    let mut iso_file: std::fs::File = File::open("../test-file/Akai CD-ROM Sound Library Volume 8.iso")?;
 
-    let bytes_read = iso_file.read(&mut buffer).expect("Failed to read bytes");
+    let target_position = SeekFrom::Start(2048);
+    
+    iso_file.seek(target_position)?;
+
+
+    let bytes_read: usize = iso_file.read(&mut buffer)?;
 
     println!("Successfully read {} bytes!\n", bytes_read);
     println!("--- HEX DUMP ---");
 
-    for byte in buffer {
+    for (i, byte) in buffer.iter().enumerate() {
 
         print!("{:02X} ", byte);
+
+
+        if (i + 1) % 16 == 0 {
+            println!();
+        }
     }
     
     println!("\n-------");
 
     
     Ok(())
-
-
-
 
 } 
 
